c8ee80d711bb3ba6ca6c0c172ff5f069081252ef
TENCENT\kobexyluo <kobexyluo@tencent.com>
2018-11-04 20:02:11 +0800

    17

---
+++ 17.打印从1到最大的n位数.py
@@ -0,0 +1,48 @@
+# coding=utf-8
+"""
+题目:
+输入数字n,按顺序打印从1到最大的n位十进制数.比如输入3,则打印出1、2、3一直到到最大的3位数999
+"""
+import sys
+
+
+def increment(number_char_array):
+    # 进位
+    carry = 0
+    is_overflow = False
+    for index in reversed(range(len(number_char_array))):
+        number = int(number_char_array[index]) + carry
+        if index == len(number_char_array) - 1:
+            number += 1
+        if number >= 10:
+            if index == 0:
+                is_overflow = True
+            number -= 10
+            number_char_array[index] = str(number)
+            carry = 1
+        else:
+            number_char_array[index] = str(number)
+            break
+    return is_overflow
+
+
+def print_number_char_array(number_char_array):
+    is_begin = False
+    for index in range(len(number_char_array)):
+        if not is_begin and number_char_array[index] != '0':
+            is_begin = True
+        if is_begin:
+            sys.stdout.write(number_char_array[index])
+    print
+
+
+def print_to_max_of_digits(n):
+    if n <= 0:
+        return
+    number_char_array = ['0' for _ in range(n)]
+    while (not increment(number_char_array)):
+        print_number_char_array(number_char_array)
+
+
+if __name__ == '__main__':
+    print_to_max_of_digits(2)
