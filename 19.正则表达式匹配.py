79dd11586d99819df0d7ab4d615a9ed276f9bbd3
lxy <442536013@qq.com>
2018-11-07 18:49:06 +0800

    19

--- 19.正则表达式匹配.py
+++ 19.正则表达式匹配.py
@@ -7,27 +7,38 @@
 
 def process(string, string_index, pattern, pattern_index):
     # basecase
+    # string 和 pattern 同时走到最后返回True
     if string_index == len(string) and pattern_index == len(pattern):
         return True
-    if string_index != len(string) and pattern_index == len(pattern):
-        return False
+    # string走到最后，但是pattern没有走到最后，返回False
     if string_index == len(string) and pattern_index != len(pattern):
         return False
+    # pattern走到最后，但是string没有走到最后，返回False
+    if string_index != len(string) and pattern_index == len(pattern):
+        return False
 
-    # 当模式的第二字符是'*'时，
+    # pattern的第二个字符是'*'
     if pattern_index < len(pattern) - 1 and pattern[pattern_index + 1] == '*':
-        # 如果模式的第一个字符和字符串的第一个字符相匹配，则在字符串后移一个，而模式有两种选择:可以在模式上向后移动两个字符，也可以保持模式不变
-        if pattern[pattern_index] == string[string_index] or pattern[pattern_index] == '.':
-            return process(string, string_index, pattern, pattern_index + 2) or \
-                   process(string, string_index + 1, pattern, pattern_index) or \
-                   process(string, string_index + 1, pattern, pattern_index + 2)
-        # 如果模式的第一个字符和字符串的第一个字符不相匹配，在模式上相后移动两个字符,这相当于'*'和前面的字符都被忽略
+        # pattern的第一个字符和string相同，或者pattern的第一个字符是'.'
+        if string[string_index] == pattern[pattern_index] or pattern[pattern_index] == '.':
+            # pattern不变，string往后移一位
+            # pattern后移两位，string不往后移
+            # pattern后移两位，string往后移一位
+            # pattern不变，string往后移一位
+            return process(string, string_index + 1, pattern, pattern_index) or \
+                   process(string, string_index, pattern, pattern_index + 2) or \
+                   process(string, string_index + 1, pattern, pattern_index + 2) or \
+                   process(string, string_index + 1, pattern, pattern_index)
+        # pattern的第一个字符和string不相同，且pattern的第一个字符不是'.'
         else:
+            # pattern后移两位，string不变
             return process(string, string_index, pattern, pattern_index + 2)
-    # 当模式的第二字符不是'*'时
+    # pattern的第二个字符不是'*'
     else:
-        if pattern[pattern_index] == string[string_index] or pattern[pattern_index] == '.':
+        # pattern的第一个字符和string相同，或者pattern的第一个字符是'.'
+        if string[string_index] == pattern[pattern_index] or pattern[pattern_index] == '.':
             return process(string, string_index + 1, pattern, pattern_index + 1)
+        # pattern的第一个字符和string不相同，且pattern的第一个字符不是'.'
         else:
             return False
 
